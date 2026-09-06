8fb8aec1adb06e7e3ec443ec1d2df97d3e9f2bb6
Protobeing <147507749+Protobeing@users.noreply.github.com>
2026-09-06 14:45:33 -0400

    Add files via upload

--- button_smasher/button_smasher.gd
+++ button_smasher/button_smasher.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var images: Array = [
+
 	preload("res://images/background-Sheet1.png"),
 	preload("res://images/background-Sheet2.png"),
 	preload("res://images/background-Sheet3.png"),
@@ -40,26 +41,69 @@ var images: Array = [
 	preload("res://images/background-Sheet36.png"),
 	preload("res://images/background-Sheet37.png"),
 	preload("res://images/background-Sheet38.png"),
+	preload("res://images/background-Sheet39.png"),
+	preload("res://images/background-Sheet40.png"),
+	preload("res://images/background-Sheet41.png"),
+	preload("res://images/background-Sheet42.png"),
+	preload("res://images/background-Sheet43.png"),
+	preload("res://images/background-Sheet43.png"),
+	preload("res://images/background-Sheet44.png"),
+	preload("res://images/background-Sheet45.png"),
+	preload("res://images/background-Sheet46.png"),
+	preload("res://images/background-Sheet47.png"),
+	preload("res://images/background-Sheet48.png"),
+	preload("res://images/background-Sheet49.png"),
+	preload("res://images/background-Sheet50.png"),
+	preload("res://images/background-Sheet51.png"),
+	preload("res://images/background-Sheet52.png"),
+	preload("res://images/background-Sheet53.png"),
+	preload("res://images/background-Sheet54.png"),
+	preload("res://images/background-Sheet55.png"),
+	preload("res://images/background-Sheet56.png"),
+	preload("res://images/background-Sheet57.png"),
+	preload("res://images/background-Sheet58.png"),
+	preload("res://images/background-Sheet59.png"),
+	preload("res://images/background-Sheet60.png"),
+	preload("res://images/background-Sheet61.png"),
+	preload("res://images/background-Sheet62.png"),
+	preload("res://images/background-Sheet63.png"),
+	preload("res://images/background-Sheet64.png"),
+	preload("res://images/background-Sheet65.png"),
+	preload("res://images/background-Sheet66.png"),
+	preload("res://images/background-Sheet67.png"),
+	preload("res://images/background-Sheet68.png"),
+	preload("res://images/background-Sheet69.png"),
+	preload("res://images/background-Sheet70.png"),
+	preload("res://images/background-Sheet71.png"),
+	preload("res://images/background-Sheet72.png"),
+	preload("res://images/background-Sheet73.png")
 	
 ]
 
+var pouring = false
 var current_index: int = 0
+var button_being_pressed = false
 
 @onready var click_button = $Button
 @onready var water_bucket = $water_bucket
+
 func _ready() -> void:
 	add_image(current_index)
 func _process(_delta: float) -> void:
 	water_bucket.global_position = get_global_mouse_position()
 func _on_button_pressed() -> void:
-
-	current_index += 1
-	if current_index >= images.size():
-		current_index = 0
-		for child in get_children():
-			if child is TextureRect:
-				child.queue_free()
-	add_image(current_index)
+	if not button_being_pressed:
+		button_being_pressed = true
+		$button_timer.start()
+		$water_bucket/CPUParticles2D.emitting = true
+		$AudioStreamPlayer2D.playing = true
+		current_index += 1
+		if current_index >= images.size():
+			current_index = 0
+			for child in get_children():
+				if child is TextureRect:
+					child.queue_free()
+		add_image(current_index)
 
 func add_image(index: int) -> void:
 	var new_image = TextureRect.new()
@@ -69,8 +113,17 @@ func add_image(index: int) -> void:
 	add_child(new_image)
 	move_child(click_button, get_child_count() - 1)
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton and not pouring:
+		pouring = true
+		$pouring.start()
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			$water_bucket/CPUParticles2D.emitting = true
 			$AudioStreamPlayer2D.playing = true
-			print("left mouse button clicked!")
+
+
+func _on_button_timer_timeout() -> void:
+	button_being_pressed = false
+
+
+func _on_pouring_timeout() -> void:
+	pouring = false
